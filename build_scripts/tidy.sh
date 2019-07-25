dc4e02bb400ee23bcf028372a2a5a44b3ccac635
Sander Valstar <sander@email.com>
2019-07-25 16:02:31 +0000

    rename

--- build_scripts/tidy.sh
+++ build_scripts/tidy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cp build_scripts/run-clang-tidy.py build/run-clang-tidy.py
+cp build_scripts/run-clang-tidy build/run-clang-tidy
 cd build
-./run-clang-tidy.py -fix -j 8 -format -p=. -export-fixes tidy_fixes.yaml '^((?!(subprojects|build|build_scripts)).)*$'
+./run-clang-tidy -fix -j 8 -format -p=. -export-fixes tidy_fixes.yaml '^((?!(subprojects|build|build_scripts)).)*$'
 cd ../
