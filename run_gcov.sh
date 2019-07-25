cf4c9e490c19f6b31efdeff523196c61dfd4ee36
Sander Valstar <sander@email.com>
2019-07-25 11:19:46 +0200

    switch to lcov and extract coverage percentages for lines, functions and branches using a python script

--- run_gcov.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "running gcovr..."
-mkdir -p build/coverage_report
-gcovr -s -r build -e subprojects -e test -o build/coverage_report/gcov_out.txt
-gcovr --html --html-details -r build -e subprojects -e test -o build/coverage_report/index.html
-
-echo "complete reports can be found under build/coverage_report"
