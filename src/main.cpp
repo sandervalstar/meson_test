48c346ae689f761f5bdd6a93cdc486b36cd9f0f9
Sander Valstar <sander@email.com>
2019-07-26 10:26:42 +0000

    adds clang-format to vscode devcontainer config

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 #define PROJECT_NAME "meson_test"
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     if (argc != 1) {
         std::cout << argv[0] << "takes no arguments.\n";
         return 1;
